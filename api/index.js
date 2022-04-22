f440762f248eb9fe178a2f6898fcc1e66d1e77cd
Ade <shennaike2386@hotmail.com>
2022-04-21 23:30:53 -0400

    created migration and seed files and also the dbconfig with knexfile

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./server')
-
-const PORT  = process.env.PORT || 9000
-
-server.listen(PORT, () => {
-    console.log(`\n**Server running on localhost:${PORT}**\n`)
-})
--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
 const server = express()
+server.use(express.json())
 
 module.exports = server;
